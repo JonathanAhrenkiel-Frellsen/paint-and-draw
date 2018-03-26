51e35ac58c8ca4950e23d98eba3bff3fe64780f6
Paulo Comasetto <paulogscomasetto@gmail.com>
2018-03-26 14:44:38 -0300

    Add files via upload

---
+++ help_text.py
@@ -0,0 +1,44 @@
+text = """
+DESENHO LIVRE
+Opcoes para alterar: cor da linha e espessura da linha(px).
+Para desenhar arraste a caneta (seta) que aparece na tela. Se quiser comecar a desenhar em outro lugar, simplesmente clique no local que a caneta ira ate la sem desenhar.
+Obs.: esse metodo pode estar sujeito a falhas.
+CIRCULO
+Opcoes para alterar: raio(graus), cor da linha, espessura da linha(px) e cor de fundo.
+Desenha um circulo aproximado no local do clique.
+POLIGONO REGULAR
+Opcoes para alterar: numero de lados, tamanho do lado(px), cor da linha, espessura da linha(px) e cor de fundo.
+Desenha um poligono regular com n lados no local do clique.
+POLIGONO IRREGULAR
+RETANGULO
+Opcoes para alterar: altura(px), comprimento(px), cor da linha, espessura da linha(px) e cor de fundo.
+Desenha um retangulo no local do clique. As medidas da altura e comprimento devem ser diferentes.
+TRAPEZIO
+Opcoes para alterar: base superior(px), base inferior(px), altura(px), cor da linha, espessura da linha(px) e cor de fundo.
+Desenha um trapezio no local do clique. As medidas das bases superior e inferior devem ser diferentes.
+LOSANGO
+Opcoes para alterar: altura(px), largura(px), cor da linha, espessura da linha(px) e cor de fundo.
+Desenha um losango no local do clique.
+TEXTO
+Opcoes para alterar: texto, fonte, tamanho da fonte(px), cor da fonte, tipo da fonte, alinhamento.
+Escreve acima do local do clique. Parametros:
+Texto: texto com, no maximo, 30 caracteres.
+Fonte: somente fontes com uma unica palavra.
+Tipo da fonte: normal, bold ou italic.
+Alinhamento: center, left ou right.
+APAGAR
+BORRACHA
+BORRACHA CIRCULAR
+Opcoes para alterar: raio(graus) e cor de fundo.
+Cria uma borracha em forma de circulo.
+A cor de fundo default e a cor de fundo da tela de desenho. A cor da linha e a mesma do fundo e a espessura e 1px.
+Nao e possivel arrastar a borracha, somente desenha-la na  tela com um unico clique.
+BORRACHA REGULAR
+Opcoes para alterar: numero de lados, tamanho do lado, cor de fundo.
+Cria uma borracha em forma de poligono regular com n lados.
+A cor de fundo padrao e a cor de fundo da tela de desenho. A cor da linha e a mesma do fundo e a espessura e 1px.
+Nao e possivel arrastar a borracha, somente desenha-la na  tela com um unico clique.
+LIMPAR TELA
+Opcoes para alterar: nova cor de fundo da tela.
+Apaga todos os desenhos da tela e altera a cor de fundo. A cor de fundo default e a cor de fundo da tela de desenho.
+"""
